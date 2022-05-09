3272254695ff98d8fb0bc1e1ec8a1dd5b3ab1800
LaNsHoR <lanshor@gmail.com>
2022-05-09 11:56:45 +0200

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const path = require('path');
 
 module.exports = {
+  mode: "development",
+  devtool: "source-map",
   entry: './html-tabs.js',
   output: {
     filename: 'html-tabs.js',
     path: path.resolve(__dirname, 'tests'),
   },
   devServer: {
-    contentBase: path.join(__dirname, 'tests'),
+    static: path.join(__dirname, 'tests'),
     compress: true,
     port: 9000
   }
